fd9bd550380f45deb69d57f04a8a110fc900cb39
Javier <javierscarione@gmail.com>
2022-09-08 23:14:12 -0300

    Assignment 2 Completed

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 const Promotions = require('./models/promotions');
-const Leaders = require('./models/leaders');
+const Leaders = require('./models/leader');
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
--- models/leader.js
+++ models/leader.js
@@ -1,26 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
-const Currency = mongoose.Types.Currency;
-
-var commentSchema = new Schema({
-    rating:  {
-        type: Number,
-        min: 1,
-        max: 5,
-        required: true
-    },
-    comment:  {
-        type: String,
-        required: true
-    },
-    author:  {
-        type: String,
-        required: true
-    }
-}, {
-    timestamps: true
-});
 
 var leaderSchema = new Schema({
     name: {
@@ -28,32 +8,26 @@ var leaderSchema = new Schema({
         required: true,
         unique: true
     },
-    description: {
-        type: String,
-        required: true
-    },
     image: {
         type: String,
         required: true
     },
-    category: {
+    designation: {
         type: String,
         required: true
     },
-    label: {
+    abbr: {
         type: String,
-        default: ''
-    },
-    price: {
-        type: Currency,
-        required: true,
-        min: 0
+        required: true
     }, 
+    description: {
+        type: String,
+        required: true
+    },
     featured: {
         type: Boolean,
         default: false
-    },
-    comments:[commentSchema]
+    }
 }, {
     timestamps: true
 });
