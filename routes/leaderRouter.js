c3f19d60d6cc4f84e55d481ad6b34b2112627019
Javier <javierscarione@gmail.com>
2022-09-12 16:16:08 -0300

    Basic Authentication

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Leaders = require('../models/leader');
+const Leaders = require('../models/leaders');
 
 const leaderRouter = express.Router();
 
