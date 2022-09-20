80da62e30776882bcdc07b6209c06b70c671f1a8
Javier <javierscarione@gmail.com>
2022-09-19 23:55:41 -0300

    Assignment 3 Complete

--- routes/users.js
+++ routes/users.js
@@ -11,9 +11,19 @@ router.use(bodyParser.json());
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/", authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
+    User.find({})
+      .then((users) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(users);
+        })
+      .catch((err) => {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({err: err});
+      });
+    });
 
 router.post('/signup', (req, res, next) => {
   User.register(new User({username: req.body.username}), 
